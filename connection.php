c462a982d9383618d5aca2a40a18639e79a7e830
RonakP06 <ronakpatel0609@gmail.com>
2018-01-19 20:01:03 -0500

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$conn = mysql_connect ($servername , $username, $password) or die("unable to connect to host");
+	sql = mysql_select_db ('test', $conn) or die("unable to connect to database");
+	
+
+
+
+
+
+?>
+</body>
+</html>
